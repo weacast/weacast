037fc21ed4fe89eb415c2d323b1ba6cae736f54d
Luc Claustres <luc.claustres@orange.fr>
2017-04-20 14:02:37 +0200

    Support usage as CLI or module

--- packages/weacast-gtiff2json/index.js
+++ packages/weacast-gtiff2json/index.js
@@ -2,12 +2,12 @@ const ps = require('geo-pixel-stream')
       fs = require('fs')
       program = require('commander')
 
-function readGeoTiff(filePath) {
+function readGeoTiff(filePath, verbose) {
   return new Promise(function(resolve, reject) {
     let readers = ps.createReadStreams(filePath)
     let blocks = []
 
-    if (program.verbose) {
+    if (verbose) {
       console.log('File metadata :')
       console.log(readers[0].metadata)
     }
@@ -27,9 +27,9 @@ function readGeoTiff(filePath) {
   })
 }
 
-function linearize(blocks, block_width, blocks_per_row) {
+function linearize(blocks, block_width, blocks_per_row, verbose) {
   let points = []
-  if (program.verbose) {
+  if (verbose) {
     console.log('Found ' + blocks.length + ' blocks of ' + blocks[0].length + ' points')
   }
   
@@ -45,43 +45,50 @@ function linearize(blocks, block_width, blocks_per_row) {
     points[i] = blocks[block][colInBlock + rowInBlock * block_width]
   }
 
-  if (program.verbose) {
+  if (verbose) {
     console.log('Linearized ' + points.length + ' points.')
   }
   return points 
 }
 
-function compress(points) {
+function compressRLE(points, round, verbose) {
   let output = []
   let currentValue = points[0], currentLen = 1
+  if (round) {
+    currentValue = Math.round(currentValue)
+  }
 
   for (let i=1, len = points.length; i<len; i++) {
-    if (points[i] == currentValue) currentLen++
+    let value = points[i]
+    if (round) {
+      value = Math.round(value)
+    }
+    if (value == currentValue) currentLen++
     else {
       output.push(currentValue)
       output.push(currentLen)
 
-      currentValue = points[i]
+      currentValue = value
       currentLen = 1
     }
   }
 
-  if (program.verbose) {
+  if (verbose) {
     console.log('Compressed to ' + output.length + ' points.')
   }
   return output
 }
 
-var gtiff2json = function(filePath) {
+var gtiff2json = function(filePath, compress, round, verbose) {
   return new Promise(function(resolve, reject) {
-    readGeoTiff(filePath).then(function(data) {
-      return linearize(data.blocks, data.metadata.blockSize.x, Math.max(1, data.metadata.width / data.metadata.blockSize.x))
+    readGeoTiff(filePath, verbose).then(function(data) {
+      return linearize(data.blocks, data.metadata.blockSize.x, Math.max(1, data.metadata.width / data.metadata.blockSize.x), verbose)
     })
     .then(function(points) {
-      if (!program.compress) {
+      if (!compress) {
         resolve(points)
       } else {
-        resolve(compress(points))
+        resolve(compressRLE(points, round, verbose))
       }
     })
     .catch(function(err) {
@@ -90,33 +97,36 @@ var gtiff2json = function(filePath) {
   })
 }
 
-module.exports = gtiff2json
+if (require.main === module) {
+  program
+    .version(require('./package.json').version)
+    .usage('<file> [options]')
+    .option('-c, --compress', 'Output RLE compressed JSON')
+    .option('-r, --round', 'Round values to nearest integer when performing RLE compression')
+    .option('-o, --output <file>', 'Output in a file instead of stdout')
+    .option('-v, --verbose', 'Verbose mode for debug purpose')
+    .parse(process.argv)
 
-program
-  .version(require('./package.json').version)
-  .usage('<file> [options]')
-  .option('-c, --compress', 'Output RLE compressed JSON')
-  .option('-o, --output <file>', 'Output in a file instead of stdout')
-  .option('-v, --verbose', 'Verbose mode for debug purpose')
-  .parse(process.argv)
+  var inputFile = program.args[0]
 
-var inputFile = program.args[0]
-
-gtiff2json(inputFile).then(function(points) {
-  if (program.output) {
-    fs.writeFile(program.output, JSON.stringify(points), function(err) {
-      if (err) {
-        console.error('Writing output file failed : ', err)
-        return
-      }
-      if (program.verbose) {
-        console.log('Wrote ' + points.length + ' points into file.')
-      }
-    })
-  } else {
-    console.log(points)
-  }
-})
-.catch(function(err) {
-  console.log(err)
-})
+  gtiff2json(inputFile, program.compress, program.round, program.verbose).then(function(points) {
+    if (program.output) {
+      fs.writeFile(program.output, JSON.stringify(points), function(err) {
+        if (err) {
+          console.error('Writing output file failed : ', err)
+          return
+        }
+        if (program.verbose) {
+          console.log('Wrote ' + points.length + ' points into file.')
+        }
+      })
+    } else {
+      console.log(points)
+    }
+  })
+  .catch(function(err) {
+    console.log(err)
+  })
+} else {
+  module.exports = gtiff2json
+}
