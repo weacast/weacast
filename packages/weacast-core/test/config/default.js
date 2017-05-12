d23d3d313ca0d818e92f3551246bb6561b684885
Luc Claustres <luc.claustres@orange.fr>
2017-05-12 15:52:52 +0200

    Added test DB

--- packages/weacast-core/test/config/default.js
+++ packages/weacast-core/test/config/default.js
@@ -23,7 +23,7 @@ module.exports = {
   db: {
     adapter: 'mongodb',
     path: path.join(__dirname, '../db-data'),
-    url: (containerized ? 'mongodb://mongodb:27017/weacast' : 'mongodb://127.0.0.1:27017/weacast')
+    url: (containerized ? 'mongodb://mongodb:27017/weacast-test' : 'mongodb://127.0.0.1:27017/weacast-test')
   },
   forecastPath: path.join(__dirname, '../forecast-data')
 }
