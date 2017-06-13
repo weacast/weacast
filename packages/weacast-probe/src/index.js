26fe1288e3a464a4caa11a71c7f6973415064bfa
Luc Claustres <luc.claustres@orange.fr>
2017-06-13 15:57:22 +0200

    Fixed missing custom event option on service creation

--- packages/weacast-probe/src/index.js
+++ packages/weacast-probe/src/index.js
@@ -3,7 +3,10 @@ import path from 'path'
 export default function init () {
   let app = this
 
-  let probesService = app.createService('probes', path.join(__dirname, 'models'), path.join(__dirname, 'services'))
+  // Setup custom events as service options
+  let probesService = app.createService('probes', path.join(__dirname, 'models'), path.join(__dirname, 'services'), {
+    events: ['results']
+  })
   app.createService('probe-results', path.join(__dirname, 'models'), path.join(__dirname, 'services'))
 
   // On startup restore listeners for forecast data updates required to update probe results
