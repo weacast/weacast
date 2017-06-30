f7d3c7a4c7f1b46595427df98bc7db9bbcf656f1
Luc Claustres <luc.claustres@orange.fr>
2017-06-30 14:40:06 +0200

    Added permission script

---
+++ packages/weacast-grib2json/set_permission.js
@@ -0,0 +1,9 @@
+const fs = require('fs-extra')
+const path = require('path')
+const os = require('os')
+
+const grib2jsonCommand = process.env.GRIB2JSON ||
+  path.join(__dirname, 'bin', os.platform() === 'win32' ? 'grib2json.cmd' : 'grib2json')
+
+// Set command executable
+fs.chmodSync(grib2jsonCommand, '110')
