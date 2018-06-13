b15704b0a9b11b122efc56c174cb7df237fbfd42
Luc Claustres <luc.claustres@orange.fr>
2018-06-13 21:15:20 +0200

    Fixed tests for https://github.com/weacast/weacast-core/issues/2

--- packages/weacast-arpege/test/index.test.js
+++ packages/weacast-arpege/test/index.test.js
@@ -32,7 +32,7 @@ describe('weacast-arpege', () => {
     service.Model.remove()
     fs.emptyDirSync(app.get('forecastPath'))
 
-    return service.updateForecastData('once')
+    return service.updateForecastData()
     .then(_ => {
       let files = fs.readdirSync(service.getDataDirectory())
       expect(files.length).to.equal(6)
