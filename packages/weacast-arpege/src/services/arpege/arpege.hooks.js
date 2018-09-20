6bbfaf280c182e28342b5d5a05d1c06f3496f47c
Luc Claustres <luc.claustres@orange.fr>
2018-09-20 16:54:57 +0200

    WIP https://github.com/weacast/weacast/issues/24

--- packages/weacast-arpege/src/services/arpege/arpege.hooks.js
+++ packages/weacast-arpege/src/services/arpege/arpege.hooks.js
@@ -1,38 +1,5 @@
-import { disallow } from 'feathers-hooks-common'
-import { hooks } from 'weacast-core'
-import authentication from 'feathers-authentication'
-const authenticate = authentication.hooks.authenticate
+import { elements } from 'weacast-core'
 
-// Marshall/Unmarshall should be always first so that we have a consistent data format in other hooks
-module.exports = {
-  before: {
-    all: [ authenticate('jwt'), hooks.marshallQuery ],
-    find: [ hooks.marshallSpatialQuery, hooks.processForecastTime ],
-    get: [],
-    create: [ disallow('external'), hooks.marshall ],
-    update: [ disallow('external'), hooks.marshall ],
-    patch: [ disallow('external'), hooks.marshall ],
-    remove: [ disallow('external') ]
-  },
+// Similar to core elements
+export default elements.hooks
 
-  after: {
-    // By default do not response with data if not explicitely asked for
-    all: [ hooks.unmarshall, hooks.processData ],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
-
-  error: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  }
-}
