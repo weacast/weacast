dabac522a3283d5afdc6eb8ac442ce3f1bb5078d
Luc Claustres <luc.claustres@orange.fr>
2019-03-15 18:30:15 +0100

    Fixed https://github.com/weacast/weacast-gtiff2json/issues/3

--- packages/weacast-gtiff2json/index.js
+++ packages/weacast-gtiff2json/index.js
@@ -115,15 +115,21 @@ if (require.main === module) {
     .usage('<file> [options]')
     .option('-c, --compress', 'Output RLE compressed JSON')
     .option('-r, --round', 'Round values to nearest integer when performing RLE compression')
+    .option('-p, --precision <precision>', 'Limit precision in JSON using the given number of digits after the decimal point', -1)
     .option('-o, --output <file>', 'Output in a file instead of stdout')
     .option('-v, --verbose', 'Verbose mode for debug purpose')
     .parse(process.argv)
 
   var inputFile = program.args[0]
+  program.precision = parseInt(program.precision)
+    
+  var numberFormatter = function (key, value) {
+    return ((program.precision >= 0) && value.toFixed) ? Number(value.toFixed(program.precision)) : value
+  }
 
   gtiff2json(inputFile, program.compress, program.round, program.verbose).then(function (points) {
     if (program.output) {
-      fs.writeFile(program.output, JSON.stringify(points), function (err) {
+      fs.writeFile(program.output, JSON.stringify(points, numberFormatter), function (err) {
         if (err) {
           console.error('Writing output file failed : ', err)
           return
