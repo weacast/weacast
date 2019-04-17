b98700b53791e50bcf650642740e367b60e1767e
Luc Claustres <luc.claustres@orange.fr>
2019-04-17 10:56:10 +0200

    Upgraded weacast-core to 1.0

--- packages/weacast-leaflet/test/index.test.js
+++ packages/weacast-leaflet/test/index.test.js
@@ -3,11 +3,6 @@ import chailint from 'chai-lint'
 import config from 'config'
 import 'jsdom-global/register'
 import fetch from 'isomorphic-fetch'
-// Importing the whole weacast module makes Leaflet time dimension fail due to jQuery not be defined
-// The following workaround, although presented as working on the internet, does not help
-// import jQuery from 'jquery'
-// window.jQuery = window.$ = jQuery
-// global.jQuery = global.$ = jQuery
 import { weacast } from 'weacast-core/client'
 
 window.fetch = fetch
