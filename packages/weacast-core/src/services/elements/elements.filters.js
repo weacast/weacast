577139e1a550cda91e4a3716740e3408607a45ad
Luc Claustres <luc.claustres@orange.fr>
2019-05-28 14:52:31 +0200

    WIP https://github.com/weacast/weacast/issues/1 (Feathers V3)

--- packages/weacast-core/src/services/elements/elements.filters.js
+++
@@ -1,6 +0,0 @@
-import _ from 'lodash'
-
-export default function (data, connection, hook) { // eslint-disable-line no-unused-vars
-  // Do not raise events for tiles
-  return (_.has(data, 'x') && _.has(data, 'y') && _.has(data, 'geometry') ? false : data)
-}
