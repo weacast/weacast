2ac0ac17f66674311ef54819fb34fe9ae27ca4aa
Luc Claustres <luc.claustres@orange.fr>
2019-05-28 14:52:47 +0200

    WIP https://github.com/weacast/weacast/issues/1 (Feathers V3)

--- packages/weacast-probe/src/services/probe-results/probe-results.filters.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (data, connection, hook) { // eslint-disable-line no-unused-vars
-  // We do not dispatch real-time events on results as we might have a lot of them
-  // FIXME : create custom events to manage this as batches
-  return false
-}
