656d027e2e86c395f2cbf4477aef955109d79d48
Luc Claustres <luc.claustres@orange.fr>
2019-05-28 17:04:45 +0200

    fix after Feathers V3 migration

--- packages/weacast-core/src/index.js
+++ packages/weacast-core/src/index.js
@@ -3,10 +3,9 @@ import services from './services'
 import initializeElements from './elements'
 import initializePlugin from './plugin'
 import weacast from './application'
-import filters from './services/elements/elements.filters'
 import hooks from './services/elements/elements.hooks'
 
-export let elements = { filters, hooks }
+export let elements = { hooks }
 export { weacast }
 export { initializeElements, initializePlugin }
 export { Database } from './db'
