146fbe1e89c1a39d188bf670d6e088c1afbd2579
Luc Claustres <luc.claustres@orange.fr>
2019-06-06 09:55:17 +0200

    Added compound index on results

--- packages/weacast-probe/src/models/probe-results.model.mongodb.js
+++ packages/weacast-probe/src/models/probe-results.model.mongodb.js
@@ -3,4 +3,5 @@ module.exports = function (app, options) {
   options.Model.ensureIndex({ forecastTime: 1 }, { expireAfterSeconds: options.expireAfter || 6 * 3600 })
   options.Model.ensureIndex({ geometry: '2dsphere' })
   options.Model.ensureIndex({ probeId: 1 })
+  options.Model.ensureIndex({ forecastTime: 1, probeId: 1 })
 }
