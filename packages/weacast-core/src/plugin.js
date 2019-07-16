5b683d52fbeeea4612dc1f3c5c38d4586c5ac770
Luc Claustres <luc.claustres@orange.fr>
2019-07-16 15:56:27 +0200

    Made initializePlugin function async

--- packages/weacast-core/src/plugin.js
+++ packages/weacast-core/src/plugin.js
@@ -2,7 +2,7 @@ import logger from 'winston'
 import initializeElements from './elements'
 
 // Create all element services
-export default function initializePlugin (app, name, servicesPath) {
+export default async function initializePlugin (app, name, servicesPath) {
   const forecasts = app.get('forecasts') ? app.get('forecasts').filter(forecast => forecast.model === name) : []
   if (!forecasts.length) {
     logger.warn('Cannot find valid ' + name + ' plugin configuration in application')
@@ -11,7 +11,7 @@ export default function initializePlugin (app, name, servicesPath) {
 
   logger.info('Initializing weacast-' + name + ' plugin')
   // Iterate over configured forecast models
-  for (let forecast of forecasts) {
-    initializeElements(app, forecast, servicesPath)
+  for (let i = 0; i < forecasts.length; i++) {
+    await initializeElements(app, forecasts[i], servicesPath)
   }
 }
