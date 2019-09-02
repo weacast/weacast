2fe9261ef9330b1ed91d7ff88cee43875aa5a235
Luc Claustres <luc.claustres@orange.fr>
2019-09-02 22:03:04 +0200

    Exported isobaric elements in default forecast configuration to ease custom configuration

--- packages/weacast-api/config/forecasts.js
+++ packages/weacast-api/config/forecasts.js
@@ -129,151 +129,181 @@ const arpegeIsobaricElements = pressureLevels.map(level =>({
   //transform: (options) => options.value - 273.15
 })))
 
+let gfs1 = {
+  name: 'gfs-world-low',
+  label: 'GFS - 1°',
+  description: 'World-wide',
+  attribution: 'Forecast data from <a href="http://www.emc.ncep.noaa.gov/index.php?branch=GFS">NCEP</a>',
+  model: 'gfs',
+  baseUrl: 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl',
+  bounds: [0, -90, 360, 90],
+  origin: [0, 90],
+  size: [360, 181],
+  resolution: [1, 1],
+  runInterval: 6 * 3600,          // Produced every 6h
+  oldestRunInterval: 24 * 3600,   // Don't go back in time older than 1 day
+  interval: 3 * 3600,             // Steps of 3h
+  lowerLimit: 0,                  // From T0
+  upperLimit: 240 * 3600,         // Up to T0+240
+  updateInterval: 15 * 60,        // Check for update every 15 minutes
+  elements: gfsGroundElements
+}
+let gfs1IsobaricElements = gfsIsobaricElements
+
+let gfs05 = {
+  name: 'gfs-world',
+  label: 'GFS - 0.5°',
+  description: 'World-wide',
+  attribution: 'Forecast data from <a href="http://www.emc.ncep.noaa.gov/index.php?branch=GFS">NCEP</a>',
+  model: 'gfs',
+  isDefault: true,
+  baseUrl: 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p50.pl',
+  bounds: [0, -90, 360, 90],
+  origin: [0, 90],
+  size: [720, 361],
+  resolution: [0.5, 0.5],
+  tileResolution: [20, 20],
+  timeseries: false,
+  runInterval: 6 * 3600,          // Produced every 6h
+  oldestRunInterval: 24 * 3600,   // Don't go back in time older than 1 day
+  interval: 3 * 3600,             // Steps of 3h
+  lowerLimit: 0,                  // From T0
+  upperLimit: 240 * 3600,         // Up to T0+240
+  updateInterval: 15 * 60,        // Check for update every 15 minutes
+  elements: gfsGroundElements
+}
+let gfs05IsobaricElements = gfsIsobaricElements
+
+let gfs025 = {
+  name: 'gfs-world-high',
+  label: 'GFS - 0.25°',
+  description: 'World-wide',
+  attribution: 'Forecast data from <a href="http://www.emc.ncep.noaa.gov/index.php?branch=GFS">NCEP</a>',
+  model: 'gfs',
+  baseUrl: 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl',
+  bounds: [0, -90, 360, 90],
+  origin: [0, 90],
+  size: [1440, 721],
+  resolution: [0.25, 0.25],
+  tileResolution: [10, 10],
+  runInterval: 6 * 3600,          // Produced every 6h
+  oldestRunInterval: 24 * 3600,   // Don't go back in time older than 1 day
+  interval: 1 * 3600,             // Steps of 1h
+  lowerLimit: 0,                  // From T0
+  upperLimit: 16 * 3600,          // Up to T0+16
+  updateInterval: 15 * 60,        // Check for update every 15 minutes
+  elements: gfsGroundElements
+}
+let gfs025IsobaricElements = gfsIsobaricElements
+
+let arpege05 = {
+  name: 'arpege-world',
+  label: 'ARPEGE - 0.5°',
+  description: 'World-wide',
+  model: 'arpege',
+  token: process.env.METEO_FRANCE_TOKEN || '__qEMDoIC2ogPRlSoRQLGUBOomaxJyxdEd__',
+  wcsBaseUrl: 'https://geoservices.meteofrance.fr/services/MF-NWP-GLOBAL-ARPEGE-05-GLOBE-WCS?SERVICE=WCS&version=2.0.1',
+  bounds: [0, -90, 360, 90],
+  origin: [0, 90],
+  size: [720, 361],
+  resolution: [0.5, 0.5],
+  tileResolution: [20, 20],
+  timeseries: false,
+  runInterval: 6 * 3600,          // Produced every 6h
+  oldestRunInterval: 24 * 3600,   // Don't go back in time older than 1 day
+  interval: 3 * 3600,             // Steps of 3h
+  lowerLimit: 0,                  // From T0
+  upperLimit: 102 * 3600,         // Up to T0+102
+  updateInterval: 15 * 60,        // Check for update every 15 minutes
+  elements: arpegeGroundElements.map(element => _.merge({ subsets: { long: [0, 360], lat: [-90, 90] } }, element))
+}
+let arpege05IsobaricElements = arpegeIsobaricElements.map(element => _.merge({ subsets: { long: [0, 360], lat: [-90, 90] } }, element))
+
+let arpege01 = {
+  name: 'arpege-europe',
+  isDefault: true,
+  label: 'ARPEGE - 0.1°',
+  description: 'Europe',
+  attribution: 'Forecast data from <a href="http://www.meteofrance.com">Météo-France</a>',
+  model: 'arpege',
+  token: '__qEMDoIC2ogPRlSoRQLGUBOomaxJyxdEd__',
+  wcsBaseUrl: 'https://geoservices.meteofrance.fr/services/MF-NWP-GLOBAL-ARPEGE-01-EUROPE-WCS?SERVICE=WCS&version=2.0.1',
+  bounds: [-32, 20, 42, 72],
+  origin: [-32, 72],
+  size: [741, 521],
+  resolution: [0.1, 0.1],
+  tileResolution: [4, 4],
+  runInterval: 6 * 3600,            // Produced every 6h
+  oldestRunInterval: 24 * 3600,     // Don't go back in time older than 1 day
+  interval: 1 * 3600,               // Steps of 1h
+  lowerLimit: 0,                    // From T0
+  upperLimit: 102 * 3600,           // Up to T0+102
+  updateInterval: 15 * 60,          // Check for update every 15 minutes
+  elements: arpegeGroundElements.map(element => _.merge({ subsets: { long: [-32, 42], lat: [20, 72] } }, element))
+}
+let arpege01IsobaricElements = arpegeIsobaricElements.map(element => _.merge({ subsets: { long: [-32, 42], lat: [20, 72] } }, element))
+
+let arome025 = {
+  name: 'arome-france',
+  label: 'AROME - 0.025°',
+  description: 'France',
+  model: 'arome',
+  token: process.env.METEO_FRANCE_TOKEN || '__qEMDoIC2ogPRlSoRQLGUBOomaxJyxdEd__',
+  wcsBaseUrl: 'https://geoservices.meteofrance.fr/services/MF-NWP-HIGHRES-AROME-0025-FRANCE-WCS?SERVICE=WCS&version=2.0.1',
+  bounds: [-8, 38, 12, 53],
+  origin: [-8, 53],
+  size: [801, 601],
+  resolution: [0.025, 0.025],
+  tileResolution: [1, 1],
+  runInterval: 3 * 3600,            // Produced every 3h
+  oldestRunInterval: 24 * 3600,     // Don't go back in time older than 1 day
+  interval: 1 * 3600,               // Steps of 1h
+  lowerLimit: 0,                    // From T0
+  upperLimit: 42 * 3600,            // Up to T0+42
+  updateInterval: 15 * 60,          // Check for update every 15 minutes
+  elements: arpegeGroundElements.map(element => _.merge({ subsets: { long: [-8, 12], lat: [38, 53] } }, element))
+}
+let arome025IsobaricElements = arpegeIsobaricElements.map(element => _.merge({ subsets: { long: [-8, 12], lat: [38, 53] } }, element))
+
+// This model generates too much data to be stored in MongoDB documents (limited to 16 MB)
+// It requires the use of the 'gridfs' data store
+let arome01 = {
+  name: 'arome-france-high',
+  label: 'AROME - 0.01°',
+  description: 'France',
+  model: 'arome',
+  token: process.env.METEO_FRANCE_TOKEN || '__qEMDoIC2ogPRlSoRQLGUBOomaxJyxdEd__',
+  wcsBaseUrl: 'https://geoservices.meteofrance.fr/services/MF-NWP-HIGHRES-AROME-001-FRANCE-WCS?SERVICE=WCS&version=2.0.1',
+  bounds: [-8, 38, 12, 53],
+  origin: [-8, 53],
+  size: [2001, 1501],
+  resolution: [0.01, 0.01],
+  tileResolution: [1, 1],
+  runInterval: 3 * 3600,            // Produced every 3h
+  oldestRunInterval: 24 * 3600,     // Don't go back in time older than 1 day
+  interval: 1 * 3600,               // Steps of 1h
+  lowerLimit: 0,                    // From T0
+  upperLimit: 42 * 3600,            // Up to T0+42
+  updateInterval: 15 * 60,          // Check for update every 15 minutes
+  elements: arpegeGroundElements.map(element => _.merge({ dataStore: 'gridfs', subsets: { long: [-8, 12], lat: [38, 53] } }, element))
+}
+let arome01IsobaricElements = arpegeIsobaricElements.map(element => _.merge({ dataStore: 'gridfs', subsets: { long: [-8, 12], lat: [38, 53] } }, element))
+
 module.exports = {
-  gfs1:  {
-    name: 'gfs-world-low',
-    label: 'GFS - 1°',
-    description: 'World-wide',
-    attribution: 'Forecast data from <a href="http://www.emc.ncep.noaa.gov/index.php?branch=GFS">NCEP</a>',
-    model: 'gfs',
-    baseUrl: 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl',
-    bounds: [0, -90, 360, 90],
-    origin: [0, 90],
-    size: [360, 181],
-    resolution: [1, 1],
-    runInterval: 6 * 3600,          // Produced every 6h
-    oldestRunInterval: 24 * 3600,   // Don't go back in time older than 1 day
-    interval: 3 * 3600,             // Steps of 3h
-    lowerLimit: 0,                  // From T0
-    upperLimit: 240 * 3600,         // Up to T0+240
-    updateInterval: 15 * 60,        // Check for update every 15 minutes
-    elements: gfsGroundElements
-  },
-  gfs05: {
-    name: 'gfs-world',
-    label: 'GFS - 0.5°',
-    description: 'World-wide',
-    attribution: 'Forecast data from <a href="http://www.emc.ncep.noaa.gov/index.php?branch=GFS">NCEP</a>',
-    model: 'gfs',
-    isDefault: true,
-    baseUrl: 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p50.pl',
-    bounds: [0, -90, 360, 90],
-    origin: [0, 90],
-    size: [720, 361],
-    resolution: [0.5, 0.5],
-    tileResolution: [20, 20],
-    timeseries: false,
-    runInterval: 6 * 3600,          // Produced every 6h
-    oldestRunInterval: 24 * 3600,   // Don't go back in time older than 1 day
-    interval: 3 * 3600,             // Steps of 3h
-    lowerLimit: 0,                  // From T0
-    upperLimit: 240 * 3600,         // Up to T0+240
-    updateInterval: 15 * 60,        // Check for update every 15 minutes
-    elements: gfsGroundElements
-  },
-  gfs025: {
-    name: 'gfs-world-high',
-    label: 'GFS - 0.25°',
-    description: 'World-wide',
-    attribution: 'Forecast data from <a href="http://www.emc.ncep.noaa.gov/index.php?branch=GFS">NCEP</a>',
-    model: 'gfs',
-    baseUrl: 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl',
-    bounds: [0, -90, 360, 90],
-    origin: [0, 90],
-    size: [1440, 721],
-    resolution: [0.25, 0.25],
-    tileResolution: [10, 10],
-    runInterval: 6 * 3600,          // Produced every 6h
-    oldestRunInterval: 24 * 3600,   // Don't go back in time older than 1 day
-    interval: 1 * 3600,             // Steps of 1h
-    lowerLimit: 0,                  // From T0
-    upperLimit: 16 * 3600,          // Up to T0+16
-    updateInterval: 15 * 60,        // Check for update every 15 minutes
-    elements: gfsGroundElements
-  },
-  arpege05: {
-    name: 'arpege-world',
-    label: 'ARPEGE - 0.5°',
-    description: 'World-wide',
-    model: 'arpege',
-    token: process.env.METEO_FRANCE_TOKEN || '__qEMDoIC2ogPRlSoRQLGUBOomaxJyxdEd__',
-    wcsBaseUrl: 'https://geoservices.meteofrance.fr/services/MF-NWP-GLOBAL-ARPEGE-05-GLOBE-WCS?SERVICE=WCS&version=2.0.1',
-    bounds: [0, -90, 360, 90],
-    origin: [0, 90],
-    size: [720, 361],
-    resolution: [0.5, 0.5],
-    tileResolution: [20, 20],
-    timeseries: false,
-    runInterval: 6 * 3600,          // Produced every 6h
-    oldestRunInterval: 24 * 3600,   // Don't go back in time older than 1 day
-    interval: 3 * 3600,             // Steps of 3h
-    lowerLimit: 0,                  // From T0
-    upperLimit: 102 * 3600,         // Up to T0+102
-    updateInterval: 15 * 60,        // Check for update every 15 minutes
-    elements: arpegeGroundElements.map(element => _.merge({ subsets: { long: [0, 360], lat: [-90, 90] } }, element))
-  },
-  arpege01: {
-    name: 'arpege-europe',
-    isDefault: true,
-    label: 'ARPEGE - 0.1°',
-    description: 'Europe',
-    attribution: 'Forecast data from <a href="http://www.meteofrance.com">Météo-France</a>',
-    model: 'arpege',
-    token: '__qEMDoIC2ogPRlSoRQLGUBOomaxJyxdEd__',
-    wcsBaseUrl: 'https://geoservices.meteofrance.fr/services/MF-NWP-GLOBAL-ARPEGE-01-EUROPE-WCS?SERVICE=WCS&version=2.0.1',
-    bounds: [-32, 20, 42, 72],
-    origin: [-32, 72],
-    size: [741, 521],
-    resolution: [0.1, 0.1],
-    tileResolution: [4, 4],
-    runInterval: 6 * 3600,            // Produced every 6h
-    oldestRunInterval: 24 * 3600,     // Don't go back in time older than 1 day
-    interval: 1 * 3600,               // Steps of 1h
-    lowerLimit: 0,                    // From T0
-    upperLimit: 102 * 3600,           // Up to T0+102
-    updateInterval: 15 * 60,          // Check for update every 15 minutes
-    elements: arpegeGroundElements.map(element => _.merge({ subsets: { long: [-32, 42], lat: [20, 72] } }, element))
-  },
-  arome025: {
-    name: 'arome-france',
-    label: 'AROME - 0.025°',
-    description: 'France',
-    model: 'arome',
-    token: process.env.METEO_FRANCE_TOKEN || '__qEMDoIC2ogPRlSoRQLGUBOomaxJyxdEd__',
-    wcsBaseUrl: 'https://geoservices.meteofrance.fr/services/MF-NWP-HIGHRES-AROME-0025-FRANCE-WCS?SERVICE=WCS&version=2.0.1',
-    bounds: [-8, 38, 12, 53],
-    origin: [-8, 53],
-    size: [801, 601],
-    resolution: [0.025, 0.025],
-    tileResolution: [1, 1],
-    runInterval: 3 * 3600,            // Produced every 3h
-    oldestRunInterval: 24 * 3600,     // Don't go back in time older than 1 day
-    interval: 1 * 3600,               // Steps of 1h
-    lowerLimit: 0,                    // From T0
-    upperLimit: 42 * 3600,            // Up to T0+42
-    updateInterval: 15 * 60,          // Check for update every 15 minutes
-    elements: arpegeGroundElements.map(element => _.merge({ subsets: { long: [-8, 12], lat: [38, 53] } }, element))
-  },
-  // This model generates too much data to be stored in MongoDB documents (limited to 16 MB)
-  // It requires the use of the 'gridfs' data store
-  arome01: {
-    name: 'arome-france-high',
-    label: 'AROME - 0.01°',
-    description: 'France',
-    model: 'arome',
-    token: process.env.METEO_FRANCE_TOKEN || '__qEMDoIC2ogPRlSoRQLGUBOomaxJyxdEd__',
-    wcsBaseUrl: 'https://geoservices.meteofrance.fr/services/MF-NWP-HIGHRES-AROME-001-FRANCE-WCS?SERVICE=WCS&version=2.0.1',
-    bounds: [-8, 38, 12, 53],
-    origin: [-8, 53],
-    size: [2001, 1501],
-    resolution: [0.01, 0.01],
-    tileResolution: [1, 1],
-    runInterval: 3 * 3600,            // Produced every 3h
-    oldestRunInterval: 24 * 3600,     // Don't go back in time older than 1 day
-    interval: 1 * 3600,               // Steps of 1h
-    lowerLimit: 0,                    // From T0
-    upperLimit: 42 * 3600,            // Up to T0+42
-    updateInterval: 15 * 60,          // Check for update every 15 minutes
-    elements: arpegeGroundElements.map(element => _.merge({ dataStore: 'gridfs', subsets: { long: [-8, 12], lat: [38, 53] } }, element))
+  gfs1,
+  gfs05,
+  gfs025,
+  arpege05,
+  arpege01,
+  arome025,
+  arome01,
+  'isobaric-elements': {
+    gfs1: gfs1IsobaricElements,
+    gfs05: gfs05IsobaricElements,
+    gfs025: gfs025IsobaricElements,
+    arpege05: arpege05IsobaricElements,
+    arpege01: arpege01IsobaricElements,
+    arome025: arome025IsobaricElements,
+    arome01: arome01IsobaricElements
   }
 }
