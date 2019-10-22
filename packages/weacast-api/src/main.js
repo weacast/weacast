2543f173d0682babc33b0457747298d8980b0442
Luc Claustres <luc.claustres@orange.fr>
2019-10-22 14:00:44 +0200

    Added launch delay env var

--- packages/weacast-api/src/main.js
+++ packages/weacast-api/src/main.js
@@ -6,26 +6,26 @@ import _ from 'lodash'
 import { Server } from './server'
 
 async function main () {
-	let server = new Server()
+  let server = new Server()
 
-	const config = server.app.get('logs')
-	const logPath = _.get(config, 'DailyRotateFile.dirname')
-	if (logPath) {
-	  // This will ensure the log directory does exist
-	  fs.ensureDirSync(logPath)
-	}
+  const config = server.app.get('logs')
+  const logPath = _.get(config, 'DailyRotateFile.dirname')
+  if (logPath) {
+    // This will ensure the log directory does exist
+    fs.ensureDirSync(logPath)
+  }
 
-	process.on('unhandledRejection', (reason, p) =>
-	  logger.error('Unhandled Rejection at: Promise ', p, reason)
-	)
+  process.on('unhandledRejection', (reason, p) =>
+    logger.error('Unhandled Rejection at: Promise ', p, reason)
+  )
 
-	await server.run()
-	logger.info('Server started listening')
+  await server.run()
+  logger.info('Server started listening')
 }
 
 if (process.env.LAUNCH_DELAY) {
-	logger.info(`Waiting ${process.env.LAUNCH_DELAY/1000}s for server to start...`)
-	setTimeout(main, process.env.LAUNCH_DELAY)
+  logger.info(`Waiting ${process.env.LAUNCH_DELAY/1000}s for server to start...`)
+  setTimeout(main, process.env.LAUNCH_DELAY)
 } else {
-	main()
+  main()
 }
