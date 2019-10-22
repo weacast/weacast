a67b5014415be508e35c5efacfacb95882253127
Luc Claustres <luc.claustres@orange.fr>
2019-10-22 13:32:56 +0200

    Added launch delay env var

--- packages/weacast-api/src/main.js
+++ packages/weacast-api/src/main.js
@@ -5,19 +5,28 @@ import logger from 'winston'
 import _ from 'lodash'
 import { Server } from './server'
 
-let server = new Server()
+function main () {
+	let server = new Server()
 
-const config = server.app.get('logs')
-const logPath = _.get(config, 'DailyRotateFile.dirname')
-if (logPath) {
-  // This will ensure the log directory does exist
-  fs.ensureDirSync(logPath)
+	const config = server.app.get('logs')
+	const logPath = _.get(config, 'DailyRotateFile.dirname')
+	if (logPath) {
+	  // This will ensure the log directory does exist
+	  fs.ensureDirSync(logPath)
+	}
+
+	process.on('unhandledRejection', (reason, p) =>
+	  logger.error('Unhandled Rejection at: Promise ', p, reason)
+	)
+
+	server.run().then(_ => {
+	  logger.info('Server started listening')
+	})
 }
 
-process.on('unhandledRejection', (reason, p) =>
-  logger.error('Unhandled Rejection at: Promise ', p, reason)
-)
-
-server.run().then(_ => {
-  logger.info('Server started listening')
-})
+if (process.env.LAUNCH_DELAY) {
+	logger.info('Waiting for server to start')
+	setTimeout(main, process.env.LAUNCH_DELAY)
+} else {
+	main()
+}
