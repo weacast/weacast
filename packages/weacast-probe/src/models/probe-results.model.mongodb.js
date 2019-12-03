2549d731dee7c53fce9645e6e24627da51316bba
Luc Claustres <luc.claustres@orange.fr>
2019-12-03 19:50:24 +0100

    feat: Allow to archive streamed probes closes https://github.com/weacast/weacast-probe/issues/16

--- packages/weacast-probe/src/models/probe-results.model.mongodb.js
+++ packages/weacast-probe/src/models/probe-results.model.mongodb.js
@@ -1,6 +1,6 @@
 module.exports = function (app, options) {
   options.Model = app.db.collection('probe-results', options.dbName)
-  options.Model.ensureIndex({ forecastTime: 1 }, { expireAfterSeconds: options.expireAfter || 6 * 3600 })
+  options.Model.ensureIndex({ forecastTime: 1 }, { expireAfterSeconds: options.expireAfter || options.ttl || 6 * 3600 })
   options.Model.ensureIndex({ geometry: '2dsphere' })
   options.Model.ensureIndex({ probeId: 1 })
   options.Model.ensureIndex({ forecastTime: 1, probeId: 1 })
