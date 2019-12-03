e05187c45d7361877b812a861346c3c4804c2a09
Luc Claustres <luc.claustres@orange.fr>
2019-12-03 19:47:41 +0100

    WIP https://github.com/weacast/weacast/issues/13 (added TTL option to forecasts/elements)

--- packages/weacast-core/src/models/elements.model.mongodb.js
+++ packages/weacast-core/src/models/elements.model.mongodb.js
@@ -3,7 +3,8 @@ module.exports = function (forecast, element, app, options) {
   // When a forecast is in the past we should not care anymore,
   // however it is still potentially valid at least until we reach the next forecast
   // Nota : adding a unique constraint on the field causes TTL not to work
-  options.Model.ensureIndex({ forecastTime: 1 }, { expireAfterSeconds: element.interval || forecast.interval })
+  options.Model.ensureIndex({ forecastTime: 1 },
+    { expireAfterSeconds: element.interval || element.ttl || forecast.interval || forecast.ttl })
   // To perform geo queries on tiles
   options.Model.ensureIndex({ geometry: '2dsphere' })
   // To perform $exists requests
