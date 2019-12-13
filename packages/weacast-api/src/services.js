0ab496a98d14b5107b49a88321d70333ea4ac1ad
Luc Claustres <luc.claustres@orange.fr>
2019-12-13 10:18:37 +0100

    feat: Make default configuration minimalist closes https://github.com/weacast/weacast-api/issues/16

--- packages/weacast-api/src/services.js
+++ packages/weacast-api/src/services.js
@@ -1,10 +1,6 @@
 import path from 'path'
-import _ from 'lodash'
-import fs from 'fs-extra'
 import logger from 'winston'
 import core, { initializeElements } from 'weacast-core'
-import probe from 'weacast-probe'
-import alert from 'weacast-alert'
 
 const modelPath = path.join(__dirname, 'models')
 const servicePath = path.join(__dirname, 'services')
@@ -27,8 +23,12 @@ module.exports = async function () {
       // Iterate over configured loaders
       for (let i = 0; i < loaders.length; i++) {
         // Setup loader plugins by dynamically require them
-        const plugin = require(`weacast-${loaders[i]}`)
-        await app.configure(plugin)
+        try {
+          const plugin = require(`weacast-${loaders[i]}`)
+          await app.configure(plugin)
+        } catch (error) {
+          logger.error(error.message)
+        }
       }
     } else { 
       // Set up only elements services otherwise
@@ -38,8 +38,18 @@ module.exports = async function () {
         await initializeElements(app, forecasts[i])
       }
     }
-    app.configure(probe)
-    app.configure(alert)
+    // Setup plugins
+    const plugins = app.get('plugins')
+    // Iterate over configured plugins
+    for (let i = 0; i < plugins.length; i++) {
+      // Setup loader plugins by dynamically require them
+      try {
+        const plugin = require(`weacast-${plugins[i]}`)
+        await app.configure(plugin)
+      } catch (error) {
+        logger.error(error.message)
+      }
+    }
   }
   catch (error) {
     logger.error(error.message)
@@ -58,70 +68,6 @@ module.exports = async function () {
           usersService.create({ email: defaultUser.email, password: defaultUser.password })
         }
       })
-    }
-  }
-
-  let probesService = app.getService('probes')
-  let alertsService = app.getService('alerts')
-
-  // Create default probes if not already done
-  let defaultProbes = app.get('defaultProbes')
-  if (defaultProbes) {
-    const defaultElementFilter = (forecast) => forecast.elements.map(element => element.name)
-    let probes = await probesService.find({ paginate: false, query: { $select: ['_id', 'name'] } })
-    for (let defaultProbe of defaultProbes) {
-      const probeName = path.parse(defaultProbe.fileName).name
-      let createdProbe = probes.find(probe => probe.name === probeName)
-      if (!createdProbe) {
-        // One probe for each forecast model and elements except if custom filter provided
-        const elementFilter = defaultProbe.filter || defaultElementFilter
-        for (let forecast of app.get('forecasts')) {
-          logger.info('Initializing default probe ' + defaultProbe.fileName + ' for forecast model ' + forecast.name)
-          let options = Object.assign({
-            name: probeName,
-            forecast: forecast.name,
-            elements: elementFilter(forecast)
-          }, defaultProbe.options)
-          if (options.elements.length > 0) {
-            let geojson = fs.readJsonSync(defaultProbe.fileName)
-            Object.assign(geojson, options)
-            const probe = await probesService.create(geojson)
-            logger.info('Initialized default probe ' + defaultProbe.fileName + ' for forecast model ' + forecast.name)
-            probes.push(probe)
-          } else {
-            logger.info('Skipping default probe ' + defaultProbe.fileName + ' for forecast model ' + forecast.name + ' (no target elements)')
-          }
-        }
-      }
-    }
-    // Create default alerts if not already done
-    let defaultAlerts = app.get('defaultAlerts')
-    if (defaultAlerts) {
-      const defaultProbeFilter = (probe) => true
-      const alerts = await alertsService.find({ paginate: false, query: { $select: ['_id', 'name'] } })
-      for (let defaultAlert of defaultAlerts) {
-        const alertName = path.parse(defaultAlert.fileName).name
-        let createdAlert = alerts.find(alert => alert.name === alertName)
-        if (!createdAlert) {
-          // One alert for each probe except if custom filter provided
-          const probeFilter = defaultAlert.filter || defaultProbeFilter
-          for (let probe of probes) {
-            if (typeof probeFilter === 'function' && !probeFilter(probe)) {
-              logger.info('Skipping default alert ' + defaultAlert.fileName + ' for probe ' + probe._id)
-              continue
-            }
-            logger.info('Initializing default alert ' + defaultAlert.fileName + ' for probe ' + probe._id)
-            let geojson = fs.readJsonSync(defaultAlert.fileName)
-            let options = Object.assign({
-              name: alertName,
-              probeId: probe._id
-            }, defaultAlert.options)
-            Object.assign(options.conditions, { geometry: { $geoWithin: { $geometry: geojson.geometry } } })
-            await alertsService.create(options)
-            logger.info('Initialized default alert ' + defaultAlert.fileName + ' for probe ' + probe._id)
-          }
-        }
-      }
     }
   }
 }
