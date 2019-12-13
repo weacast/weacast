7c08af2849aa564a43240c17598f4ab517c6b216
Luc Claustres <luc.claustres@orange.fr>
2019-12-13 10:17:18 +0100

    WIP https://github.com/weacast/weacast-api/issues/16 (moved init code to module)

--- packages/weacast-alert/src/index.js
+++ packages/weacast-alert/src/index.js
@@ -1,4 +1,6 @@
 import path from 'path'
+import fs from 'fs-extra'
+import logger from 'winston'
 import makeDebug from 'debug'
 
 const debug = makeDebug('weacast:weacast-alert')
@@ -13,6 +15,37 @@ export default async function init () {
   	path.join(__dirname, 'services'), Object.assign({
     events: ['alerts']
   }, app.getServiceOptions('alerts')))
+
+  // Create default alerts if not already done
+  let defaultAlerts = app.get('defaultAlerts')
+  if (defaultAlerts) {
+    const defaultProbeFilter = (probe) => true
+    const alerts = await alertsService.find({ paginate: false, query: { $select: ['_id', 'name'] } })
+    for (let defaultAlert of defaultAlerts) {
+      const alertName = path.parse(defaultAlert.fileName).name
+      let createdAlert = alerts.find(alert => alert.name === alertName)
+      if (!createdAlert) {
+        // One alert for each probe except if custom filter provided
+        let probes = await app.getService('probes').find({ paginate: false, query: { $select: ['_id', 'name'] } })
+        const probeFilter = defaultAlert.filter || defaultProbeFilter
+        for (let probe of probes) {
+          if (typeof probeFilter === 'function' && !probeFilter(probe)) {
+            logger.info('Skipping default alert ' + defaultAlert.fileName + ' for probe ' + probe._id)
+            continue
+          }
+          logger.info('Initializing default alert ' + defaultAlert.fileName + ' for probe ' + probe._id)
+          let geojson = fs.readJsonSync(defaultAlert.fileName)
+          let options = Object.assign({
+            name: alertName,
+            probeId: probe._id
+          }, defaultAlert.options)
+          Object.assign(options.conditions, { geometry: { $geoWithin: { $geometry: geojson.geometry } } })
+          await alertsService.create(options)
+          logger.info('Initialized default alert ' + defaultAlert.fileName + ' for probe ' + probe._id)
+        }
+      }
+    }
+  }
 
   // On startup restore alerts CRON tasks
   if (alertsService) {
