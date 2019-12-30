7604bb0e6b7dc61b96f5ac16b64efbfdbe4e7ec9
Luc Claustres <luc.claustres@orange.fr>
2019-12-30 13:03:29 +0100

    Fixed lint

--- packages/weacast-core/src/services/index.js
+++ packages/weacast-core/src/services/index.js
@@ -2,7 +2,7 @@ import path from 'path'
 
 module.exports = function () {
   const app = this
-  
+
   app.createService('forecasts',
     path.join(__dirname, '..', 'models'),
     path.join(__dirname, '..', 'services'),
