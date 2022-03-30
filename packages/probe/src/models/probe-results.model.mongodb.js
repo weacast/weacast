0f1e89863d42c08650ca42b6f52a36e458d88335
daffl <daff@neyeon.com>
2022-03-30 15:28:00 -0700

    chore: Finalize upgrades and fix additional tests

--- packages/probe/src/models/probe-results.model.mongodb.js
+++ packages/probe/src/models/probe-results.model.mongodb.js
@@ -1,7 +1,7 @@
 module.exports = function (app, options) {
   options.Model = app.db.collection('probe-results', options.dbName)
-  options.Model.ensureIndex({ forecastTime: 1 }, { expireAfterSeconds: options.expireAfter || options.ttl || 6 * 3600 })
-  options.Model.ensureIndex({ geometry: '2dsphere' })
-  options.Model.ensureIndex({ probeId: 1 })
-  options.Model.ensureIndex({ forecastTime: 1, probeId: 1 })
+  options.Model.createIndex({ forecastTime: 1 }, { expireAfterSeconds: options.expireAfter || options.ttl || 6 * 3600 })
+  options.Model.createIndex({ geometry: '2dsphere' })
+  options.Model.createIndex({ probeId: 1 })
+  options.Model.createIndex({ forecastTime: 1, probeId: 1 })
 }
