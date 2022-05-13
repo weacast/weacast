dd67b57f07793318dc891fe4065f6121ef1dd4cb
cnouguier <christophe.nouguier@free.fr>
2022-05-13 19:56:01 +0200

    wip: Upgrade NodeJS to v16: fixed index.js in leafet package #2

--- packages/leaflet/src/index.js
+++ packages/leaflet/src/index.js
@@ -1,9 +1,2 @@
-import { ForecastLayer, FlowLayer, HeatLayer, ScalarLayer, WindBarbIcon } from './layers.js'
-import { GridRenderer } from './grid-renderer.js'
-// A shorter version of all of this should be the following
-/*
-export * as hooks from './hooks'
-*/
-// However for now we face a bug in babel so that transform-runtime with export * from 'x' generates import statements in transpiled code
-// Tracked here : https://github.com/babel/babel/issues/2877
-export { ForecastLayer, FlowLayer, HeatLayer, ScalarLayer, WindBarbIcon, GridRenderer }
+export * from './layers/index.js'
+export * from './grid-renderer.js'
--- packages/leaflet/src/layers/index.js
+++ packages/leaflet/src/layers/index.js
@@ -1,13 +1,4 @@
-import { ForecastLayer } from './forecast-layer.js'
-import { FlowLayer } from './flow-layer.js'
-import { ScalarLayer } from './scalar-layer.js'
-import { WindBarbIcon } from './windbarb.js'
-
-const layers = {
-  ForecastLayer,
-  FlowLayer,
-  ScalarLayer,
-  WindBarbIcon
-}
-
-export default layers
+export * from './forecast-layer.js'
+export * from './flow-layer.js'
+export * from './scalar-layer.js'
+export * from './windbarb.js'
