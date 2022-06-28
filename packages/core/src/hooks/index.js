7ea2874577396eea0bea55a540cac6ac9b417ca8
Luc Claustres <luc.claustres@orange.fr>
2022-06-28 16:21:26 +0200

    chore: fixed skip event hook

--- packages/core/src/hooks/index.js
+++ packages/core/src/hooks/index.js
@@ -1,11 +1,8 @@
-import feathers from '@feathersjs/feathers'
-
-const { SKIP } = feathers
-
 export * from './marshall.js'
 export * from './query.js'
 export * from './logger.js'
 
 export function skipEvents (hook) {
-  return SKIP
+  hook.event = null
+  return hook
 }
