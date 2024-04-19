f051b123eabff416827991e4a73069af2df33dc9
Luc Claustres <luc.claustres@orange.fr>
2024-04-19 19:21:28 +0200

    wip: Refactor CI to use GitHub actions and generic bash scripts - removed travis CI and added c8 config (#75)

--- .travis.env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build docker with version number only on release
-if [[ -z "$TRAVIS_TAG" ]]
-then
-	export VERSION=latest
-else
-	export VERSION=$(node -p -e "require('./packages/api/package.json').version")
-fi
--- .travis.test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [[ $TRAVIS_COMMIT_MESSAGE == *"[skip test]"* ]] || [[ -n "$TRAVIS_TAG" ]]
-then
-	echo "Skipping test stage"
-else
-	source .travis.env.sh
-	yarn install
-	yarn test
-fi
